019209fe9448a7b54768f303a2e0150bff60a69f
Victor Heshiki <victor_heshiki@hotmail.com>
2022-10-29 18:16:14 -0300

    test: ensure SignUpController returns 400 if no name provider

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
 export default {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*/ts'],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
+    roots: ['<rootDir>/src'],
+    collectCoverageFrom: ['<rootDir>/src/**/*/ts'],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'node',
+    transform: {
+        '.+\\.ts$': 'ts-jest'
+    }
 }
