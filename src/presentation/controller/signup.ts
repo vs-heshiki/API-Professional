22f7090152c13c679605ab16254b8f237fc41f19
Victor Heshiki <victor_heshiki@hotmail.com>
2022-10-29 21:06:33 -0300

    feat: ensure SignUp Controller do not allow a missing password

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -4,7 +4,7 @@ import { HttpResponse, HttpRequest } from '../protocols/http'
 
 export class SignUpController {
     handle (httpRequest: HttpRequest): HttpResponse {
-        const fieldRequired = ['name', 'email']
+        const fieldRequired = ['name', 'email', 'password']
         for (const field of fieldRequired) {
             if (!httpRequest.body[field]) {
                 return badRequest(new MissingParamError(field))
