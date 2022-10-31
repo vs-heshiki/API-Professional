715e56a155f42d16ba6c06fd580d3d8327d74f88
Victor Heshiki <victor_heshiki@hotmail.com>
2022-10-31 19:04:56 -0300

    refactor: remove code duplicated with destructuring

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -17,12 +17,13 @@ export class SignUpController implements Controller {
                     return badRequest(new MissingParamError(field))
                 }
             }
+            const { password, confirmPassword, email } = httpRequest.body
 
-            if (httpRequest.body.password !== httpRequest.body.confirmPassword) {
+            if (password !== confirmPassword) {
                 return badRequest(new InvalidParamError('confirmPassword'))
             }
 
-            const isValid = this.emailValidator.isValid(httpRequest.body.email)
+            const isValid = this.emailValidator.isValid(email)
             if (!isValid) {
                 return badRequest(new InvalidParamError('email'))
             }
