6a92a042159306e100484bd8653817278c5f2e80
Victor Heshiki <victor_heshiki@hotmail.com>
2022-10-31 18:53:18 -0300

    feature: ensure SignUpController return 400 if password is different from password confirmation

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -17,6 +17,11 @@ export class SignUpController implements Controller {
                     return badRequest(new MissingParamError(field))
                 }
             }
+
+            if (httpRequest.body.password !== httpRequest.body.confirmPassword) {
+                return badRequest(new InvalidParamError('confirmPassword'))
+            }
+
             const isValid = this.emailValidator.isValid(httpRequest.body.email)
             if (!isValid) {
                 return badRequest(new InvalidParamError('email'))
