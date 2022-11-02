c1951791341ef95d443740e4307bd6a673ea2826
Victor Heshiki <victor_heshiki@hotmail.com>
2022-11-02 19:51:38 -0300

    feat: ensure SignUpController return 201 when correct values is provider

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -30,11 +30,15 @@ export class SignUpController implements Controller {
                 return badRequest(new InvalidParamError('email'))
             }
 
-            this.addAccount.add({
+            const account = this.addAccount.add({
                 name,
                 email,
                 password
             })
+            return {
+                statusCode: 201,
+                body: account
+            }
         } catch (err) {
             return serverError()
         }
