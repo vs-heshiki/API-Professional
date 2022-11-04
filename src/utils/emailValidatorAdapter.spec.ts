abb155dff6b80663097eddeaeaa8c6fc6f81c484
Victor Heshiki <victor_heshiki@hotmail.com>
2022-11-04 18:26:01 -0300

    tet: ensure EmailValidator return true if validator returns true

---
+++ src/utils/emailValidatorAdapter.spec.ts
@@ -0,0 +1,23 @@
+import { EmailValidatorAdapter } from './emailValidator'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+    isEmail (): boolean {
+        return true
+    }
+}))
+
+describe('EmailValidator Adapter', () => {
+    test('Should return false if EmailValidator returns false', () => {
+        const sut = new EmailValidatorAdapter()
+        jest.spyOn(validator, 'isEmail').mockReturnValueOnce(true)
+        const isValid = sut.isValid('any_invalid@email.com')
+        expect(isValid).toBe(true)
+    })
+
+    test('Should return true if EmailValidator returns true', () => {
+        const sut = new EmailValidatorAdapter()
+        const isValid = sut.isValid('any_valid@email.com')
+        expect(isValid).toBe(true)
+    })
+})
