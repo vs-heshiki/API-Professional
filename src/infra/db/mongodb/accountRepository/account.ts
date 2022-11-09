8c6165ea74b8873d7da095229c4dfabb0b341736
Victor Heshiki <victor_heshiki@hotmail.com>
2022-11-08 22:30:03 -0300

    refactor: move map to MongoHelper

--- src/infra/db/mongodb/accountRepository/account.ts
+++ src/infra/db/mongodb/accountRepository/account.ts
@@ -4,8 +4,7 @@ import { MongoHelper } from '../helper/mongoHelper'
 export class AccountMongoRepository implements AddAccountRepository {
     async add (accountData: AddAccountModel): Promise<AccountModel> {
         const accountCollection = MongoHelper.getCollection('accounts')
-        const result = await accountCollection.insertOne(accountData)
-        const account = { id: result.insertedId.toString(), ...accountData }
-        return account
+        await accountCollection.insertOne(accountData)
+        return MongoHelper.map(accountData)
     }
 }
--- src/infra/db/mongodb/helper/mongoHelper.ts
+++ src/infra/db/mongodb/helper/mongoHelper.ts
@@ -13,5 +13,13 @@ export const MongoHelper = {
 
     getCollection (name: string): Collection {
         return this.client.db().collection(name)
+    },
+
+    map (data: any): any {
+        const { _id, ...rest } = data
+        return {
+            ...rest,
+            id: _id
+        }
     }
 }
