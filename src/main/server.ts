b7e393c6f4c5a4fbdc1f84b4ca7545a2110c0e8c
Victor Heshiki <victor_heshiki@hotmail.com>
2022-11-09 17:44:37 -0300

    feat: ensure BodyParser Middleware parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,3 @@
-import express from 'express'
-const app = express()
-
-app.get('/', (req, res) => res.send('Hello World!'))
+import app from './config/app'
 
 app.listen(8080, () => console.log('Listening in 8080 port'))
