ed324cb35b22acd157db7f98b4f720aacf102f08
Victor Heshiki <victor_heshiki@hotmail.com>
2022-11-14 20:14:10 -0300

    refactory: move sut creation to a factory method

--- src/tests/specs/log.spec.ts
+++ src/tests/specs/log.spec.ts
@@ -1,11 +1,15 @@
 import { LogControllerDecorator } from '../../main/decorators/log'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 
-describe('LogController Decorator', () => {
-    test('Should call controller handle', async () => {
-        class ControllerStub implements Controller {
-            async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-                const httpResponse: HttpResponse = {
+interface sutTypes {
+    sut: LogControllerDecorator
+    controllerStub: Controller
+}
+
+const newController = (): Controller => {
+    class ControllerStub implements Controller {
+        async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+            const httpResponse: HttpResponse = {
                 statusCode: 200,
                 body: {
                     name: 'anyName',
@@ -13,13 +17,22 @@ describe('LogController Decorator', () => {
                     password: '12345',
                     confirmPassword: '12345'
                 }
-                }
-                return new Promise(resolve => resolve(httpResponse))
             }
+            return new Promise(resolve => resolve(httpResponse))
         }
+    }
+    return new ControllerStub()
+}
 
-        const controllerStub = new ControllerStub()
-        const sut = new LogControllerDecorator(controllerStub)
+const newSut = (): sutTypes => {
+    const controllerStub = newController()
+    const sut = new LogControllerDecorator(controllerStub)
+    return { sut, controllerStub }
+}
+
+describe('LogController Decorator', () => {
+    test('Should call controller handle', async () => {
+        const { sut, controllerStub } = newSut()
         const handleSpyOn = jest.spyOn(controllerStub, 'handle')
         const httpRequest = {
             body: {
