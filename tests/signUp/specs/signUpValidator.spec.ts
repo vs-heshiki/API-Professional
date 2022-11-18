823ad15112f819983858da3e9ab986d2c36afd0a
Victor Heshiki <victor_heshiki@hotmail.com>
2022-11-18 17:13:23 -0300

    test: move EmailValidator to ValidatorComposite

---
+++ tests/signUp/specs/signUpValidator.spec.ts
@@ -0,0 +1,29 @@
+import { EmailValidator } from '../../../src/presentation/protocols/emailValidator'
+import { Validator, ValidatorInputRequired, ValidatorCompareInputs, ValidatorEmail } from '../../../src/presentation/helpers/validator'
+import { ValidatorComposite } from '../../../src/presentation/helpers/validator/validatorComposite'
+import { newSignUpValidator } from '../../../src/main/factories/signupValidator'
+
+jest.mock('../../../src/presentation/helpers/validator/validatorComposite')
+
+const newEmailValidator = (): EmailValidator => {
+    class EmailValidatorStub implements EmailValidator {
+        isValid (email: string): boolean {
+            return true
+        }
+    }
+    return new EmailValidatorStub()
+}
+
+describe('SignUpValidator Factory', () => {
+    test('Should call Validator Composite with all Validators', () => {
+        newSignUpValidator()
+        const validations: Validator[] = []
+        const inputs = ['name', 'email', 'password', 'confirmPassword']
+        for (const field of inputs) {
+            validations.push(new ValidatorInputRequired(field))
+        }
+        validations.push(new ValidatorCompareInputs('password','confirmPassword'))
+        validations.push(new ValidatorEmail('email', newEmailValidator()))
+        expect(ValidatorComposite).toHaveBeenCalledWith(validations)
+    })
+})
---
+++ tests/signUp/specs/validatorEmail.spec.ts
@@ -0,0 +1,50 @@
+import { InvalidParamError } from '../../../src/presentation/errors/invalidParamError'
+import { ValidatorEmail } from '../../../src/presentation/helpers/validator/validatorEmail'
+import { EmailValidator } from '../../../src/presentation/protocols/emailValidator'
+
+const newEmailValidator = (): EmailValidator => {
+    class EmailValidatorStub implements EmailValidator {
+        isValid (email: string): boolean {
+            return true
+        }
+    }
+    return new EmailValidatorStub()
+}
+
+interface SutTypes {
+    sut: ValidatorEmail
+    emailValidatorStub: EmailValidator
+}
+
+const newSut = (): SutTypes => {
+    const emailValidatorStub = newEmailValidator()
+    const sut = new ValidatorEmail('email', emailValidatorStub)
+    return {
+        sut,
+        emailValidatorStub
+    }
+}
+
+describe('Validator Email', () => {
+    test('Should call EmailValidator with correct email', () => {
+        const { sut, emailValidatorStub } = newSut()
+        const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+        sut.validate({ email: 'any_email@email.com' })
+        expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com')
+    })
+
+    test('Should return an error if EmailValidator return false', () => {
+        const { sut, emailValidatorStub } = newSut()
+        jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+        const invalidEmail = sut.validate('any_invalid@email.com')
+        expect(invalidEmail).toEqual(new InvalidParamError('email'))
+    })
+
+    test('Should throw if EmailValidator throws', () => {
+        const { sut, emailValidatorStub } = newSut()
+        jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+            throw new Error()
+        })
+        expect(sut.validate).toThrow()
+    })
+})
