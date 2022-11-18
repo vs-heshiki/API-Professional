7dc87cccb3f513b2a51172699482998a590d487c
Victor Heshiki <victor_heshiki@hotmail.com>
2022-11-18 18:06:30 -0300

    test: ensure ValidatorCompareInputs not return if validation pass

---
+++ tests/signUp/specs/validatorCompareFields.spec.ts
@@ -0,0 +1,20 @@
+import { ValidatorCompareInputs } from './../../../src/presentation/helpers/validator/validatorCompareInputs'
+import { InvalidParamError } from './../../../src/presentation/errors/invalidParamError'
+
+describe('Validator Compare Fields', () => {
+    const newSut = (): ValidatorCompareInputs => {
+        return new ValidatorCompareInputs('input','otherInput')
+    }
+
+    test('Should return a InvalidParamError if validation fails', () => {
+        const sut = newSut()
+        const error = sut.validate({ input: 'value', otherInput: 'otherValue' })
+        expect(error).toEqual(new InvalidParamError('otherInput'))
+    })
+
+    test('Should not return if validation pass', () => {
+        const sut = newSut()
+        const error = sut.validate({ input: 'value', otherInput: 'value' })
+        expect(error).toBeFalsy()
+    })
+})
