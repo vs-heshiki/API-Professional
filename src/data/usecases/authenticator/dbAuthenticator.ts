d8db20de6939196f05af866f117ea3f9ab57cf03
Victor Heshiki <victor_heshiki@hotmail.com>
2022-11-19 20:15:23 -0300

    feat: ensure DbAuthenticator returns an access token on success

--- src/data/usecases/authenticator/dbAuthenticator.ts
+++ src/data/usecases/authenticator/dbAuthenticator.ts
@@ -1,20 +1,31 @@
 import { HashCompare } from '../../protocols/cryptography/hashCompare'
 import { LoadAccountByEmailRepository } from '../../protocols/db/loadAccountByEmailRepository'
 import { Authenticate, AuthenticateModel } from './../../../domain/usecases/authenticate'
+import { TokenGenerator } from './../../protocols/cryptography/tokenGenerator'
 
 export class DbAuthenticator implements Authenticate {
     private readonly loadAccountByEmail: LoadAccountByEmailRepository
     private readonly hashCompare: HashCompare
+    private readonly tokenGenerator: TokenGenerator
 
-    constructor (loadAccountByEmail: LoadAccountByEmailRepository, hashCompare: HashCompare) {
+    constructor (
+        loadAccountByEmail: LoadAccountByEmailRepository,
+        hashCompare: HashCompare,
+        tokenGenerator: TokenGenerator
+    ) {
         this.loadAccountByEmail = loadAccountByEmail
         this.hashCompare = hashCompare
+        this.tokenGenerator = tokenGenerator
     }
 
     async auth (authenticate: AuthenticateModel): Promise<string> {
         const account = await this.loadAccountByEmail.load(authenticate.email)
         if (account) {
-            await this.hashCompare.compare(authenticate.password, account.password)
+            const compare = await this.hashCompare.compare(authenticate.password, account.password)
+            if (compare) {
+                const accessToken = await this.tokenGenerator.generate(account.id)
+                return accessToken
+            }
         }
         return null
     }
