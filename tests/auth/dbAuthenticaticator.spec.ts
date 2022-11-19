ae71ffb1a3700f27d7ebaef3c5b50a121ffabaa5
Victor Heshiki <victor_heshiki@hotmail.com>
2022-11-19 20:32:42 -0300

    test: ensure DbAuthenticator throw if UpdateAccessTokenRepository throws

--- tests/auth/dbAuthenticaticator.spec.ts
+++ tests/auth/dbAuthenticaticator.spec.ts
@@ -153,4 +153,12 @@ describe('DB Authenticator UseCase', () => {
         await sut.auth(newFakeAuthenticate())
         expect(updateSpyOn).toHaveBeenCalledWith('any_id', 'access_token')
     })
+
+    test('Should throw if UpdateAccessTokenRepository throws', async () => {
+        const { sut, updateAccessTokenRepositoryStub } = newSut()
+        jest.spyOn(updateAccessTokenRepositoryStub, 'update')
+            .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+        const promise = sut.auth(newFakeAuthenticate())
+        await expect(promise).rejects.toThrow()
+    })
 })
