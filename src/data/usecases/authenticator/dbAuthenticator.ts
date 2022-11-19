a63a3c2c0f29aa237f44c64b2e81306183c4c7ee
Victor Heshiki <victor_heshiki@hotmail.com>
2022-11-19 20:30:20 -0300

    feat: ensure DbAuthenticator call UpdateAccessTokenRepository with correct values

--- src/data/usecases/authenticator/dbAuthenticator.ts
+++ src/data/usecases/authenticator/dbAuthenticator.ts
@@ -1,3 +1,4 @@
+import { UpdateAccessTokenRepository } from './../../protocols/cryptography/updateAccessTokenRepository'
 import { HashCompare } from '../../protocols/cryptography/hashCompare'
 import { LoadAccountByEmailRepository } from '../../protocols/db/loadAccountByEmailRepository'
 import { Authenticate, AuthenticateModel } from './../../../domain/usecases/authenticate'
@@ -7,15 +8,18 @@ export class DbAuthenticator implements Authenticate {
     private readonly loadAccountByEmail: LoadAccountByEmailRepository
     private readonly hashCompare: HashCompare
     private readonly tokenGenerator: TokenGenerator
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
 
     constructor (
         loadAccountByEmail: LoadAccountByEmailRepository,
         hashCompare: HashCompare,
-        tokenGenerator: TokenGenerator
+        tokenGenerator: TokenGenerator,
+        updateAccessTokenRepository: UpdateAccessTokenRepository
     ) {
         this.loadAccountByEmail = loadAccountByEmail
         this.hashCompare = hashCompare
         this.tokenGenerator = tokenGenerator
+        this.updateAccessTokenRepository = updateAccessTokenRepository
     }
 
     async auth (authenticate: AuthenticateModel): Promise<string> {
@@ -24,6 +28,7 @@ export class DbAuthenticator implements Authenticate {
             const compare = await this.hashCompare.compare(authenticate.password, account.password)
             if (compare) {
                 const accessToken = await this.tokenGenerator.generate(account.id)
+                await this.updateAccessTokenRepository.update(account.id, accessToken)
                 return accessToken
             }
         }
