d35d35fa423a8bd9478da11c2f62113dc94d42df
Victor Heshiki <victor_heshiki@hotmail.com>
2022-11-21 19:57:17 -0300

    test: ensure JwtAdapter return an AccessToken on success

--- tests/cryptography/jwt/jwtAdapter.spec.ts
+++ tests/cryptography/jwt/jwtAdapter.spec.ts
@@ -1,6 +1,12 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from '../../../src/infra/cryptography/jwt/jwtAdapter'
 
+jest.mock('jsonwebtoken', () => ({
+    async sign (): Promise<string> {
+        return new Promise(resolve => resolve('any_token'))
+    }
+}))
+
 interface SutTypes {
     sut: JwtAdapter
 }
@@ -13,10 +19,16 @@ const newSut = (): SutTypes => {
 }
 
 describe('JWT Adapter', () => {
-    test('Should call getToken with correct values', async () => {
+    test('Should call JWTAdapter with correct values', async () => {
         const { sut } = newSut()
         const signSpyOn = jest.spyOn(jwt, 'sign')
         await sut.encrypt('any_id')
         expect(signSpyOn).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+    })
+
+    test('Should return a AccessToken on sign success', async () => {
+        const { sut } = newSut()
+        const accessToken = await sut.encrypt('any_id')
+        expect(accessToken).toBe('any_token')
     })
 })
