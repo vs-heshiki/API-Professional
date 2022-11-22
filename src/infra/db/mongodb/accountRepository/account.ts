f3f4797a76e3b71cb1cab2b866a27f93d41091d1
Victor Heshiki <victor_heshiki@hotmail.com>
2022-11-22 16:42:35 -0300

    feat: ensure MongoDBAdapter return an account on loadByEmail success

--- src/infra/db/mongodb/accountRepository/account.ts
+++ src/infra/db/mongodb/accountRepository/account.ts
@@ -1,10 +1,17 @@
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/loadAccountByEmailRepository'
 import { AddAccountRepository, AddAccountModel, AccountModel } from '../../../../data/usecases/addAccount/dbAddAccountProtocols'
 import { MongoHelper } from '../helper/mongoHelper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
     async add (accountData: AddAccountModel): Promise<AccountModel> {
         const accountCollection = await MongoHelper.getCollection('accounts')
         await accountCollection.insertOne(accountData)
         return MongoHelper.map(accountData)
+    }
+
+    async loadAccByEmail (email: string): Promise<AccountModel> {
+        const accountCollection = await MongoHelper.getCollection('accounts')
+        const account = await accountCollection.findOne({ email })
+        return MongoHelper.map(account)
     }
 }
