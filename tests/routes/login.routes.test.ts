c99a0d0c1fccb0590e578e96cd81a2549acb60df
Victor Heshiki <victor_heshiki@hotmail.com>
2022-11-23 17:28:11 -0300

    test: add login route

--- tests/routes/login.routes.test.ts
+++ tests/routes/login.routes.test.ts
@@ -1,6 +1,16 @@
 import { MongoHelper } from '../../src/infra/db/mongodb/helper/mongoHelper'
+import { Collection } from 'mongodb'
+import bcrypt from 'bcrypt'
 import request from 'supertest'
 import app from '../../src/main/config/app'
+
+const genSalt = async (): Promise<string> => {
+    const saltRounds = 10
+    const salt = await bcrypt.genSalt(saltRounds)
+    return salt
+}
+
+let getCollection: Collection
 
 beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -11,7 +21,7 @@ afterAll(async () => {
 })
 
 beforeEach(async () => {
-    const getCollection = await MongoHelper.getCollection('accounts')
+    getCollection = await MongoHelper.getCollection('accounts')
     await getCollection.deleteMany({})
 })
 
@@ -25,6 +35,24 @@ describe('Login Routes', () => {
                     email: 'victor.heshiki@gmail.com',
                     password: '123',
                     confirmPassword: '123'
+                })
+                .expect(200)
+        })
+    })
+
+    describe('Login Route', () => {
+        test('Should return 200 on login', async () => {
+            const password = await bcrypt.hash('123', await genSalt())
+            await getCollection.insertOne({
+                name: 'Victor',
+                email: 'victor.heshiki@gmail.com',
+                password
+            })
+            await request(app)
+                .post('/api/login')
+                .send({
+                    email: 'victor.heshiki@gmail.com',
+                    password: '123'
                 })
                 .expect(200)
         })
