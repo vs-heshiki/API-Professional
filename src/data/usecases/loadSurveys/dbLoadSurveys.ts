73fde9e691b8f3f249bf2a98b5b5f2db538b72a6
Victor Heshiki <victor_heshiki@hotmail.com>
2022-12-19 22:18:52 -0300

    feat: ensure DbLoadSurveys returns a list of surveys on success

--- src/data/usecases/loadSurveys/dbLoadSurveys.ts
+++ src/data/usecases/loadSurveys/dbLoadSurveys.ts
@@ -8,7 +8,7 @@ export class DbLoadSurveys implements LoadSurveys {
     ) {}
 
     async load (): Promise<SurveyModel[]> {
-        await this.loadSurveyRepository.loadAll()
-        return []
+        const surveys = await this.loadSurveyRepository.loadAll()
+        return surveys
     }
 }
