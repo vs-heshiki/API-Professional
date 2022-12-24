3401dfb83190ab232383825f990412252782677e
Victor Heshiki <victor_heshiki@hotmail.com>
2022-12-24 19:49:58 -0300

    test: ensure SurveyResultMongoRepository update a survey result if already exists

--- tests/infra/mongodb/surveyResultMongoRepository.spec.ts
+++ tests/infra/mongodb/surveyResultMongoRepository.spec.ts
@@ -1,4 +1,4 @@
-import { Collection } from 'mongodb'
+import { Collection, ObjectId } from 'mongodb'
 import { MongoHelper } from '@/infra/db/mongodb/helper/mongoHelper'
 import { SurveyResultMongoRepository } from '@/infra/db/mongodb/surveyResult/surveyResultMongoRepository'
 import { AccountModel } from '@/domain/model/accountModel'
@@ -50,8 +50,10 @@ describe('Survey Result MongoDB Repository', () => {
     beforeEach(async () => {
         surveyCollection = await MongoHelper.getCollection('surveys')
         await surveyCollection.deleteMany({})
+
         surveyResultCollection = await MongoHelper.getCollection('surveyResults')
         await surveyResultCollection.deleteMany({})
+
         accountCollection = await MongoHelper.getCollection('accounts')
         await accountCollection.deleteMany({})
     })
@@ -61,15 +63,43 @@ describe('Survey Result MongoDB Repository', () => {
             const account = await mockAccount()
             const survey = await mockSurvey()
             const sut = newSut()
-            const surveyResult = await sut.save({
+            await sut.save({
                 surveyId: survey.id,
-                userId: account.id,
+                accountId: account.id,
                 answer: survey.answers[0].answer,
                 date: new Date()
             })
+            const surveyResult = await surveyResultCollection.findOne({
+                surveyId: new ObjectId(survey.id),
+                accountId: new ObjectId(account.id)
+            })
             expect(surveyResult).toBeTruthy()
-            expect(surveyResult.id).toBeTruthy()
-            expect(surveyResult.answer).toBe(survey.answers[0].answer)
+        })
+
+        test('Should update a result survey if already exists', async () => {
+            const account = await mockAccount()
+            const survey = await mockSurvey()
+            await surveyResultCollection.insertOne({
+                surveyId: new ObjectId(survey.id),
+                accountId: new ObjectId(account.id),
+                answer: survey.answers[1].answer,
+                date: new Date()
+            })
+            const sut = newSut()
+            await sut.save({
+                surveyId: survey.id,
+                accountId: account.id,
+                answer: survey.answers[0].answer,
+                date: new Date()
+            })
+            const surveyResult = await surveyResultCollection
+                .find({
+                    surveyId: new ObjectId(survey.id),
+                    accountId: new ObjectId(account.id)
+                })
+                .toArray()
+            expect(surveyResult).toBeTruthy()
+            expect(surveyResult.length).toBe(1)
         })
     })
 })
