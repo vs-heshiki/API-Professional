39514cffd10668c9b9579b3ec5bf0d452134b306
Victor Heshiki <victor_heshiki@hotmail.com>
2022-12-25 02:05:47 -0300

    refactor: rename file with Factory in the end

--- src/main/routes/survey.routes.ts
+++ src/main/routes/survey.routes.ts
@@ -1,5 +1,5 @@
 import { ExpressRouterAdapter } from '@/main/adapters/expressRouterAdapter'
-import { newLoadSurveysController } from '@/main/factories/controllers/survey/loadSurveys/loadSurveysController'
+import { newLoadSurveysController } from '@/main/factories/controllers/survey/loadSurveys/loadSurveysControllerFactory'
 import { newAddSurveyController } from '@/main/factories/controllers/survey/addSurvey/addSurveyControllerFactory'
 import { adminAuth, auth } from '@/main/middlewares/auth'
 import { Router } from 'express'
