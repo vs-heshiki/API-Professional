b96bdc1b36e1d83c3c290b3b94ee9738071a7366
Victor Heshiki <victor_heshiki@hotmail.com>
2022-12-26 22:20:16 -0300

    feat: add swagger config and apply no cache middleware

---
+++ src/main/config/swaggerConfig.ts
@@ -0,0 +1,8 @@
+import { Express } from 'express'
+import { serve, setup } from 'swagger-ui-express'
+import { noCache } from '@/main/middlewares/noCache'
+import swaggerConfig from '@/main/docs'
+
+export default (app: Express): void => {
+    app.use('/api-docs', noCache, serve, setup(swaggerConfig))
+}
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+    openapi: '3.0.3',
+    info: {
+        title: 'API Professional',
+        description: 'API Rest based in Typescript, follow TDD design, SOLID principles and Clean Architecture',
+        version: '1.0.0'
+    }
+}
