42f4d274eac31b16147183ecf98b880a71c20e54
Victor Heshiki <victor_heshiki@hotmail.com>
2022-12-31 18:56:55 -0300

    test: ensure LoadSurveyResultController return 500 if LoadSurveyById throws

--- tests/presentation/survey/loadSurveyResultController.spec.ts
+++ tests/presentation/survey/loadSurveyResultController.spec.ts
@@ -1,8 +1,8 @@
 import { LoadSurveyResultController } from '@/presentation/controller/surveyResults/loadSurveyResult/loadSurveyResultController'
 import { LoadSurveyById } from '@/presentation/controller/surveyResults/saveSurveyResult/saveSurveyResultControllerProtocols'
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden } from '@/presentation/helpers/http/httpHelpers'
-import { mockSurveyResultRequest } from '@/tests/mocks'
+import { forbidden, serverError } from '@/presentation/helpers/http/httpHelpers'
+import { mockSurveyResultRequest, throwError } from '@/tests/mocks'
 import { mockLoadSurveyById } from '@/tests/presentation/survey/stubs/surveyStubs'
 
 type SutTypes = {
@@ -32,5 +32,12 @@ describe('LoadSurveyResult Controller', () => {
         jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(null)
         const httpResponse = await sut.handle(mockSurveyResultRequest())
         expect(httpResponse).toEqual(forbidden(new InvalidParamError('any_survey_id')))
+    })
+
+    test('Should return 500 if LoadSurveyById throws', async () => {
+        const { sut, loadSurveyByIdStub } = newSut()
+        jest.spyOn(loadSurveyByIdStub, 'loadById').mockImplementationOnce(throwError)
+        const httpResponse = await sut.handle(mockSurveyResultRequest())
+        expect(httpResponse).toEqual(serverError(new Error()))
     })
 })
