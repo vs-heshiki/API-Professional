3b1e35658289fbfd5634ab73727d1c28d6de8819
Victor Heshiki <victor_heshiki@hotmail.com>
2022-12-31 18:56:06 -0300

    feat: ensure LoadSurveyResultController return 500 if LoadSurveyById throws

--- src/presentation/controller/surveyResults/loadSurveyResult/loadSurveyResultController.ts
+++ src/presentation/controller/surveyResults/loadSurveyResult/loadSurveyResultController.ts
@@ -1,4 +1,4 @@
-import { forbidden } from '@/presentation/helpers/http/httpHelpers'
+import { forbidden, serverError } from '@/presentation/helpers/http/httpHelpers'
 import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './loadSurveyResultControllerProtocols'
 import { InvalidParamError } from '@/presentation/errors'
 
@@ -8,11 +8,15 @@ export class LoadSurveyResultController implements Controller {
     ) { }
 
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
             const { surveyId } = httpRequest.params
             const survey = await this.loadSurveyById.loadById(surveyId)
             if (!survey) {
                 return forbidden(new InvalidParamError(surveyId))
             }
             return null
+        } catch (err) {
+            return serverError(err)
+        }
     }
 }
