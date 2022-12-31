a4b379c8dd5c319d917feac40e674f3e0a69c5a6
Victor Heshiki <victor_heshiki@hotmail.com>
2022-12-31 19:38:38 -0300

    docs: add load sruvey result docs

--- src/main/docs/paths/surveyResultPath.ts
+++ src/main/docs/paths/surveyResultPath.ts
@@ -49,5 +49,42 @@ export const surveyResultPath = {
                 $ref: '#/components/notFound'
             }
         }
+    },
+    get: {
+        security: [{
+            apiKeyAuth: []
+        }],
+        tags: ['Survey'],
+        summary: 'API to load a survey result',
+        parameters: [{
+            name: 'surveyId',
+            in: 'path',
+            description: 'Survey Id',
+            required: true,
+            schema: {
+                type: 'string'
+            }
+        }],
+        responses: {
+            200: {
+                description: 'Success',
+                content: {
+                    'application/json': {
+                        schema: {
+                            $ref: '#/schemas/surveyResult'
+                        }
+                    }
+                }
+            },
+            403: {
+                $ref: '#/components/forbidden'
+            },
+            500: {
+                $ref: '#/components/serverError'
+            },
+            404: {
+                $ref: '#/components/notFound'
+            }
+        }
     }
 }
